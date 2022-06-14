75f22f975ef282b60adf0f14afbcbcbadf283a38
Adeola Ephraim <dunsin.bada@gmail.com>
2022-06-14 16:10:31 +0100

    init app

---
+++ app/Models/TenantPost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TenantPost extends Model
+{
+    use HasFactory;
+}
